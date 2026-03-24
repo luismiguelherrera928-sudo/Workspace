69de625ac4a9b22757b68db080728fbaa127c793
luismiguelherrera928-sudo <luismiguelherrera928@gmail.com>
2026-03-24 21:14:12 +0000

    24-03

---
+++ 24-03/ejercicio1-1.js
@@ -0,0 +1,7 @@
+var nombre="miguel"
+let edad=17
+const ciudad="medellin"
+console.log(nombre);
+console.log("edad:",edad);
+console.log("ciudad:",ciudad);
+console.log("nombre:",nombre,"edad:",edad,"ciudad:",ciudad);
